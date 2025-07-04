8919a262fd5bd5ba1dca8a4d1b3a639e916f01b6
PRABIL J <GKS1446@GKS-INFOTECH.COM>
2025-07-04 21:34:31 +0530

    comment the sigup for testing

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Login from './components/User/Login/login';
 import Signup from './components/User/Signup/Signup';
 import OTPVerification from './components/User/Verification/OTPVerification';
-import EnterEmail from './components/User/Verification/EnterEmail';
-// import ProductList from './components/User/products/product';
+import EnterEmail from './components/User/Verification/EnterEmail'; import ProductList from './components/User/products/product';
 import { PrivateRoutes, AdminPrivateRoutes } from './components/protectedRoutes/PrivateRoutes'
 import ProductView from './components/User/products/ProductView';
 import Wishlist from './components/User/Wishlist/Wishlist';
@@ -39,7 +38,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/signup" element={<Signup />} /> */}
         <Route path='/verifyOTP' element={<OTPVerification />} />
         <Route path='/emailEnter' element={<EnterEmail />} />
 
